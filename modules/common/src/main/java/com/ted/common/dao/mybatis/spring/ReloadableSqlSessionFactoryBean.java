e5fa60e9c757f216d806f64603c8c1fa58be6593
ghostsite <ghostzhang@gmail.com>
2013-03-11 17:21:07 +0800

    mybatis dynamic sql template ok ,fix bug

--- modules/common/src/main/java/com/ted/common/dao/mybatis/spring/ReloadableSqlSessionFactoryBean.java
+++ modules/common/src/main/java/com/ted/common/dao/mybatis/spring/ReloadableSqlSessionFactoryBean.java
@@ -3,21 +3,26 @@ package com.ted.common.dao.mybatis.spring;
 import static org.springframework.util.ObjectUtils.isEmpty;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Collection;
+import java.util.Map;
 
 import org.apache.ibatis.builder.xml.XMLMapperBuilder;
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.core.io.Resource;
 import org.springside.modules.utils.Reflections;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.ted.common.dao.mybatis.session.ReloadableSqlSessionFactory;
 
+/**
+ * 这种只支持通过SqlSessionTemplate.java调用的方法，不支持接口映射XML的DAO 
+ */
 public class ReloadableSqlSessionFactoryBean extends SqlSessionFactoryBean {
-    private static final List<ResourceHolder> resourceHolderList = Lists.newCopyOnWriteArrayList(); //string 是文件的名字，由于不能得不到namedQL到file的映射，故全文件扫描
-    private long                              delay              = 10;                             //准备配置在xml中用的,秒
+    private static final Map<Resource, ResourceHolder> resourceHolderMap = Maps.newConcurrentMap(); //string 是文件的名字，由于不能得不到namedQL到file的映射，故全文件扫描
+    private long                                       delay             = 10;                     //准备配置在xml中用的,秒
 
     @Override
     protected SqlSessionFactory buildSqlSessionFactory() throws IOException {
@@ -51,41 +56,67 @@ public class ReloadableSqlSessionFactoryBean extends SqlSessionFactoryBean {
      */
     protected void load2ResourceHolder(Resource resource) {
         try {
-            resourceHolderList.add(new ResourceHolder(resource, resource.getFile().lastModified()));
+            resourceHolderMap.put(resource, new ResourceHolder(resource, resource.getFile().lastModified()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    };
 
     //参照 TemplateDaoSupport.java
     public void refresh() {//全文件扫描,因为找不到匹配的方法
         final long delayTime = this.getDelay();
 
         if (delayTime > 0) {//如果delayTime <= 0, 则不reload
-            for (ResourceHolder resourceHolder : resourceHolderList) {
-                if (resourceChanged(resourceHolder)) {
+            for (ResourceHolder resourceHolder : resourceHolderMap.values()) {
+                if (resourceChangedDelayTime(resourceHolder)) {
                     refreshMapping(resourceHolder);
                 }
             }
         }
     };
 
-    //刷新load变化的Resource到Configuration
+    //刷新load变化的Resource到Configuration, 并且刷新resourceHolderMap的时间。
     private void refreshMapping(ResourceHolder resourceHolder) {
         try {
             SqlSessionFactory sessionFactory = (SqlSessionFactory) Reflections.getFieldValue(this, "sqlSessionFactory");//木办法，只能强制获取
             Configuration configuration = sessionFactory.getConfiguration();
+
+            removeMappedStatementInConfiguration(configuration, getMappedStatement(resourceHolder));
+
             XMLMapperBuilder xmlMapperBuilder = new XMLMapperBuilder(resourceHolder.getResource().getInputStream(), configuration, configuration.toString(), configuration.getSqlFragments());
             xmlMapperBuilder.parse();
+
+            resourceHolderMap.get(resourceHolder.getResource()).lastModified = resourceHolder.getResource().getFile().lastModified();
         } catch (Exception e) {
             e.printStackTrace();
         }
     };
 
-    //变化了then return true
-    private boolean resourceChanged(ResourceHolder resourceHolder) {
+    private Collection<MappedStatement> getMappedStatement(ResourceHolder resourceHolder) {
+        Configuration configuration = new Configuration();
+        XMLMapperBuilder xmlMapperBuilder = null;
         try {
-            if (resourceHolder.lastModified < resourceHolder.getResource().getFile().lastModified()) {
+            xmlMapperBuilder = new XMLMapperBuilder(resourceHolder.getResource().getInputStream(), configuration, configuration.toString(), configuration.getSqlFragments());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (xmlMapperBuilder != null) {
+            xmlMapperBuilder.parse();
+        }
+        return configuration.getMappedStatements();
+    }
+
+    public void removeMappedStatementInConfiguration(Configuration configuration, Collection<MappedStatement> mappedStatemetCollection) {
+        Map<String, MappedStatement> mappedStatements = (Map<String, MappedStatement>) Reflections.getFieldValue(configuration, "mappedStatements");//被逼的
+        for (MappedStatement statement : mappedStatemetCollection) {
+            mappedStatements.remove(statement.getId());
+        }
+    }
+
+    //变化了then return true
+    private boolean resourceChangedDelayTime(ResourceHolder resourceHolder) {
+        try {
+            if (resourceHolder.lastModified + getDelay() * 1000 < resourceHolder.getResource().getFile().lastModified()) {
                 return true;
             }
         } catch (IOException e) {
