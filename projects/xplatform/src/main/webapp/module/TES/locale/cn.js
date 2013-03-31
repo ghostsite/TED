1fa72dc4c42b8a774f91535a4d83b371ecb90de6
ghostsite <ghostzhang@gmail.com>
2013-03-31 13:44:04 +0800

    i18n for notification

--- projects/xplatform/src/main/webapp/module/TES/locale/cn.js
+++ projects/xplatform/src/main/webapp/module/TES/locale/cn.js
@@ -4,15 +4,24 @@ T({
 	},
 	Caption : {
 		Other : {
-			"Part Cmf" : "Part Cmf"
+			"Org" : "组织机构"
 		},
 
 		Button : {
-			
+			"Generate" : "生成",
+			"Save" : "保存"
+		},
+		
+		Menu : {
+			TES:{
+				view:{
+					"Notification":"提醒文字"
+				}
+			},
+			"test":"例子"
 		}
 	},
 
 	Message : {
-
 	}
 });
