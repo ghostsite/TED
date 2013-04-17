44abb12f565407a0c5e4157b4025cfe8b0dc4465
ghostsite <ghostzhang@gmail.com>
2013-04-17 14:40:23 +0800

    BUS FileResourceList的下载的check

--- projects/xplatform/src/main/webapp/module/BUS/controller/fileresource/FileResourceList.js
+++ projects/xplatform/src/main/webapp/module/BUS/controller/fileresource/FileResourceList.js
@@ -17,14 +17,32 @@ Ext.define('BUS.controller.fileresource.FileResourceList', {
 
 	// 下载
 	doDownload : function(view, record) {
-		//TODO check authority that file can be downloaed
-		//check()
-		
-		var path = 'fileresource/download/' + record.data.id;
-		SF.exporter.doDownload(path);
+		if(this.checkCanDownload()){
+			var path = 'fileresource/download/' + record.data.id;
+			SF.exporter.doDownload(path);
+		}
 	},
 
 	showPic : function(view, record) {
 		Ext.getCmp('showfilepicforbus').getEl().dom.src = 'fileresource/downloadPic/' + record.data.id;// 显示图片
+	},
+	
+	checkCanDownload  : function(){
+		var fileCode = SF.getSelectedValueFromGrid(this.getGrid(),'code');
+		var callConfig ={
+			url: 'fileresource/currentUserCanDownload',
+			params:{
+				fileCode : fileCode
+			}
+		}
+		
+		var canDownload = SF.cf.callServiceSync(callConfig);
+		
+		if (canDownload.result.data == false) {
+			SF.alertWarn('警告', '您没有权限下载此文件!');
+			return false;
+		}else{
+			true;
+		}
 	}
 });
